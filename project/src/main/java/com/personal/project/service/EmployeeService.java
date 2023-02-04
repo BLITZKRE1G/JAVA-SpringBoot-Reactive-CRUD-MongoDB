314f00ee8c384409661628c836127796998488f0
blitz-23 <94664667+blitz-23@users.noreply.github.com>
2023-02-04 10:43:03 +0530

    Service Update
    Update the Service File.

--- project/src/main/java/com/personal/project/service/EmployeeService.java
+++ project/src/main/java/com/personal/project/service/EmployeeService.java
@@ -37,6 +37,13 @@ public class EmployeeService {
         return repository.findById(_id);
     }
 
+    public Flux<Employee> getAllEmployees() {
+        return repository.findAll().flatMap(employee -> {
+            log.info("Found: " + employee);
+            return Flux.just(employee);
+        });
+    }
+
     public Mono<Employee> saveEmployeeToDatabase(Employee employee) {
         return repository.save(employee);
     }
@@ -63,44 +70,55 @@ public class EmployeeService {
 
     public Flux<Employee> getEmployeesByLocation(String location) {
         return repository.findByLocation(location).switchIfEmpty(Mono.defer(() -> {
+            log.info("Employees DON'T exist from this Location.");
             throw new RuntimeException("Employees DON'T exist from this Location.");
         }));
     }
 
     public Flux<Employee> getEmployeesByRole(String role) {
         return repository.findByRole(role).switchIfEmpty(Mono.defer(() -> {
+            log.info("Employees DON'T exist for this Role.");
             throw new RuntimeException("Employees DON'T exist for this Role.");
         }));
     }
 
-    public Flux<Employee> getByEmploymentStatus(String employmentStatus){
-        return repository.findByStatus(employmentStatus);
+    public Flux<Employee> getEmployeesByDepartment(String department){
+        return repository.findByDept(department)
+                .switchIfEmpty(Mono.defer(()->{
+                    log.info("Employees DON'T Exist for this Role!");
+                    throw new RuntimeException("Employees DON'T Exist for this Role!");
+                }));
     }
 
-    public Mono<PaginatedEmployee> getEmployeesByPagination(EmployeeFilter employeeFilter, String _id){
-        if(_id == null){
+    public Flux<Employee> getByEmploymentStatus(boolean employed) {
+        return repository.findByEmployed(employed);
+    }
+
+    public Mono<PaginatedEmployee> findEmployeesByPagination(EmployeeFilter employeeFilter, String _id) {
+        if (_id == null) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "ID Should NOT be NULL!");
         }
-        if(employeeFilter.getPage() == null || employeeFilter.getPage()<1){
+        if (employeeFilter.getPage() == null || employeeFilter.getPage() < 1) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Page should NOT be NULL or LESS than 1!");
         }
-        if(employeeFilter.getCount() == null || employeeFilter.getCount()<10){
+        if (employeeFilter.getCount() == null || employeeFilter.getCount() < 10) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Count Should be GREATER than 10!");
         }
 
-        Pageable page = PageRequest.of(employeeFilter.getPage()-1, employeeFilter.getCount(), Sort.by("salary"));
+        Pageable page = PageRequest.of(employeeFilter.getPage() - 1, employeeFilter.getCount(), Sort.by("salary"));
 
-        if(employeeFilter.getSortValue() != null && employeeFilter.getSortOrder() != null){
-            page = PageRequest.of(employeeFilter.getPage() - 1, employeeFilter.getCount(), Sort.by(getSortingOrders(employeeFilter.getSortValue(), employeeFilter.getSortOrder())));
+        if (employeeFilter.getSortValue() != null && employeeFilter.getSortOrder() != null) {
+            page = PageRequest.of(employeeFilter.getPage() - 1, employeeFilter.getCount(),
+                    Sort.by(getSortingOrders(employeeFilter.getSortValue(), employeeFilter.getSortOrder())));
         }
 
         return employeeFilterRepository.filterEmployee(employeeFilter, _id, page);
     }
 
-    public List<Order> getSortingOrders(List<String> values, List<Direction> orders){
+    public List<Order> getSortingOrders(List<String> values, List<Direction> orders) {
         List<Order> sorts = new ArrayList<>();
 
-        for(int i=0; i<values.size(); i++){
+        for (int i = 0; i < values.size(); i++) {
             sorts.add(new Order(orders.get(i), values.get(i)));
         }
 
